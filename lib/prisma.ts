6f3f694c1a5233e386854d55b09f8002aa9cf36f
Khalidmasudaliyu <168911969+usmanmasud@users.noreply.github.com>
2026-06-28 16:28:37 +0100

    log fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
 // @ts-ignore — generated after `prisma generate`
 import { PrismaClient } from "@prisma/client";
+import { createPgAdapter } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma: InstanceType<typeof PrismaClient> };
 
 export const prisma: InstanceType<typeof PrismaClient> =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ?? new PrismaClient({
+    adapter: createPgAdapter({
+      connectionString: process.env.DATABASE_URL,
+    }),
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
