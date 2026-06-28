36d3ed93025f5d5fe8a4d454052902b16e0437e0
Khalidmasudaliyu <168911969+usmanmasud@users.noreply.github.com>
2026-06-28 15:54:26 +0100

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,6 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = globalThis as unknown as { prisma: InstanceType<typeof PrismaClient> };
 
 export const prisma: InstanceType<typeof PrismaClient> =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ?? new PrismaClient({ datasourceUrl: process.env.DATABASE_URL });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
